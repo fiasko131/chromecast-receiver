2486b9954543e0e792be19cf1a400634a941f3d0
fiasko131 <pierre.steff@gmail.com>
2025-09-07 14:04:23 +0200

    modif index et js3

--- receiver.js
+++ receiver.js
@@ -1,35 +1,37 @@
-const context = cast.framework.CastReceiverContext.getInstance();
-const playerManager = context.getPlayerManager();
+document.addEventListener('DOMContentLoaded', () => {
+    // Ce message de log s'affichera dès que le DOM est prêt.
+    console.log('DOM est prêt, initialisation du récepteur Cast...');
+    
+    const context = cast.framework.CastReceiverContext.getInstance();
+    const playerManager = context.getPlayerManager();
 
-// Utiliser l'événement onReady pour garantir que le framework est initialisé
-context.onReady = () => {
-  playerManager.setMessageInterceptor(
-    cast.framework.messages.MessageType.LOAD,
-    loadRequestData => {
-      if (loadRequestData.media && loadRequestData.media.customData) {
-        const { customData } = loadRequestData.media;
-        console.log('En-têtes personnalisés reçus:', customData.headers);
-        loadRequestData.media.customData = customData;
-      }
-      return loadRequestData;
-    }
-  );
+    playerManager.setMessageInterceptor(
+        cast.framework.messages.MessageType.LOAD,
+        loadRequestData => {
+            if (loadRequestData.media && loadRequestData.media.customData) {
+                const { customData } = loadRequestData.media;
+                console.log('En-têtes personnalisés reçus:', customData.headers);
+                loadRequestData.media.customData = customData;
+            }
+            return loadRequestData;
+        }
+    );
 
-  playerManager.addEventListener(
-    cast.framework.events.EventType.PLAYING,
-    () => {
-      console.log('Lecture du média commencée, passage en mode plein écran.');
-      document.body.classList.add('playing');
-    }
-  );
+    playerManager.addEventListener(
+        cast.framework.events.EventType.PLAYING,
+        () => {
+            console.log('Lecture du média commencée, passage en mode plein écran.');
+            document.body.classList.add('playing');
+        }
+    );
 
-  playerManager.addEventListener(
-    cast.framework.events.EventType.IDLE,
-    () => {
-      console.log('Lecture du média arrêtée, retour au mode par défaut.');
-      document.body.classList.remove('playing');
-    }
-  );
+    playerManager.addEventListener(
+        cast.framework.events.EventType.IDLE,
+        () => {
+            console.log('Lecture du média arrêtée, retour au mode par défaut.');
+            document.body.classList.remove('playing');
+        }
+    );
 
-  context.start();
-};
+    context.start();
+});
