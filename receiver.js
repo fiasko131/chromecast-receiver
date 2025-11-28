db9fbdc12039443ccb1795c752ce437512bcc827
fiasko131 <pierre.steff@gmail.com>
2025-11-28 09:35:27 +0100

    add custom commands

--- receiver.js
+++ receiver.js
@@ -1103,6 +1103,7 @@ function handlePlayerState(state) {
       if (pauseIcon) pauseIcon.style.display = "none";
       if (audioPauseIcon) audioPauseIcon.style.display = "none";
       audioUI.style.display = "none";
+      if (document.getElementById("player")) document.getElementById("player").style.display = "none";
       break;
   }
 }
